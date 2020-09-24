95990398385ba4c276aa5c6c4b60866f902ae2c4
James Dai <K038246@eltham-college.org.uk>
2020-09-24 12:43:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Flight_Planning_NEA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AirportData;
+            AirportData = File.ReadAllText("Airports.txt");
+            Console.WriteLine(AirportData);
+        }
+    }
+}
+
